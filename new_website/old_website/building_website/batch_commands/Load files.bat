f218db7d53b968f7a726926b8a587752829e3ae5
Armand_laptop <517226@edu.rocmn.nl>
2018-01-19 09:06:30 +0100

    setup new work eviorment for the new website

---
+++ new_website/old_website/building_website/batch_commands/Load files.bat
@@ -0,0 +1,24 @@
+@ECHO off
+
+ECHO cd to map location
+	CD ../jekyll
+
+ECHO start sass listener
+	START cmd /k "sass --watch _sass/index.scss:css/main.css"
+
+ECHO start jekyll listener
+	START cmd /k "jekyll serve"
+
+ECHO open project map
+	START explorer .
+
+ECHO open the project in browser
+	START http://127.0.0.1:4000/
+
+ECHO start github console
+	CD C:\Users\gebruiker\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GitHub, Inc
+	github
+
+TIMEOUT 60
+PAUSE
+
