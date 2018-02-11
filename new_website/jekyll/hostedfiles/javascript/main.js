e803b3334018a1c98e9c8c7b15f893b17c8f3c9f
Armaniimus_home <Tommy_chester@hotmail.com>
2018-02-11 22:07:54 +0100

    ajax works hosted files

--- new_website/jekyll/hostedfiles/javascript/main.js
+++ new_website/jekyll/hostedfiles/javascript/main.js
@@ -2,4 +2,4 @@ const alerter = function(res) {
     alert(res);
 }
 
-ajax_module.get("php/main.php", "", alerter)
+ajax_module.get("php/main.php", {mode:"readDir", url:"programming2018/ass000"}, alerter)
--- new_website/jekyll/hostedfiles/php/main.php
+++ new_website/jekyll/hostedfiles/php/main.php
@@ -1,1 +1,23 @@
-<?php echo "succes"; ?>
+<?php
+
+if (isset($_GET["mode"]) ) {
+    if (isset($_GET["url"]) && ($_GET["mode"] === "readDir" || $_GET["mode"] === 0) ) {
+        include_once "php_partials/scan_dir_crud.php";
+        $return = scannedDir("../files/" . $_GET["url"]);
+    }
+}
+
+
+
+
+
+
+
+
+if (isset($return)){
+    echo json_encode($return);
+} else {
+    echo false;
+}
+
+?>
