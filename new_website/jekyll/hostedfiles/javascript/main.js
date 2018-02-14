6d22ff03bc23ff83a4ea6a168b362110b20384a6
Armand_laptop <517226@edu.rocmn.nl>
2018-02-14 19:45:24 +0100

    added file system shizzle

--- new_website/jekyll/hostedfiles/javascript/main.js
+++ new_website/jekyll/hostedfiles/javascript/main.js
@@ -1,5 +1,22 @@
 const fileModule = (function() {
-    
+    const navigationObject = {
+        children: {},       //= contains children
+        url: "",            //= contains url to this file
+        extension: "",      //= contains extension
+        folder: false,      //= holds boolean to check if its a folder(true) or not (false)
+        exectuteAble: false //= holds boolean to check if it is executeable (true) or not (false)
+    }
+
+    function p_navObjectController() {
+
+    }
+
+    return {
+        navObjectController: p_navObjectController() {
+            
+        }
+    }
+
 })();
 
 const onLoad = (function() {
